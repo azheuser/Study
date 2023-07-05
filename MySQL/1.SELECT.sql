17decb576758c3d452e3a87a9638576e9b384d5b
Azhe <2592836740@qq.com>
2023-07-05 20:36:11 +0800

    select 语句

--- MySQL/1.SELECT.sql
+++ MySQL/1.SELECT.sql
@@ -8,3 +8,18 @@ FROM products;
 
 SELECT *
 FROM products;
+
+# 限制输出不同的行
+SELECT DISTINCT vend_id
+FROM products;
+
+# 限制输出前多少行
+SELECT prod_name
+FROM products
+LIMIT 5;
+
+# LIMIT 开始行 行数
+SELECT prod_name
+FROM products
+LIMIT 0, 5;
+# LIMIT 5 OFFSET 0
